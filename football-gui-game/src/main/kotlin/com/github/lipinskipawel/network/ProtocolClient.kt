162ca2e8d2e40813e9ad4441163b85034f7a5983
Pawel Lipinski <plipinski@atlassian.com>
2020-09-20 09:43:05 +0200

    Change default setting of Protocol segment size

--- football-gui-game/src/main/kotlin/com/github/lipinskipawel/network/ProtocolClient.kt
+++ football-gui-game/src/main/kotlin/com/github/lipinskipawel/network/ProtocolClient.kt
@@ -1,12 +1,22 @@
 package com.github.lipinskipawel.network
 
+/**
+ * This class is responsible for converting data into proper Protocol format.
+ * This class is Thread safe.
+ * Protocol format:
+ * | version of protocol | number of segments | data |
+ *
+ * Size of each field:
+ * version of protocol - 1 byte
+ * number of segments  - 1 byte
+ * data                - 2048 bytes
+ */
 internal class ProtocolClient {
 
     companion object {
-        private val META_HEAD_JAVA_SERIALIZED_OBJECTS: ByteArray = ByteArray(1) { 0x01 }
-        private val META_HEAD_STRING: ByteArray = ByteArray(1) { 0x02 }
-        private val SEGMENT_SIZE: Int = 127
-        private val NUMBER_OF_SEGMENT: Int = 127
+        private val PROTOCOL_VERSION: ByteArray = ByteArray(1) { 0x01 }
+        private val SEGMENT_SIZE: Int = 2048
+        private val NUMBER_OF_SEGMENT: Int = 100
 
         fun createClient(): ProtocolClient {
             return ProtocolClient()
@@ -22,9 +32,9 @@ internal class ProtocolClient {
         val divided = sizeOfData / SEGMENT_SIZE
         val isTheNumberIsNatural = sizeOfData % SEGMENT_SIZE == 0
         return if (isTheNumberIsNatural) {
-            META_HEAD_STRING + computeNumberOfSegmentsNeeded(divided)
+            PROTOCOL_VERSION + computeNumberOfSegmentsNeeded(divided)
         } else {
-            META_HEAD_STRING + computeNumberOfSegmentsNeeded(divided + 1)
+            PROTOCOL_VERSION + computeNumberOfSegmentsNeeded(divided + 1)
         }
     }
 
--- football-gui-game/src/test/kotlin/com/github/lipinskipawel/network/ProtocolClientTest.kt
+++ football-gui-game/src/test/kotlin/com/github/lipinskipawel/network/ProtocolClientTest.kt
@@ -7,13 +7,13 @@ internal class ProtocolClientTest {
 
     @Test
     @ExperimentalStdlibApi
-    fun shouldConvertStringToProperFormat() {
+    fun shouldConvertStringToProtocolFormat() {
         val message = "Hello world!"
         val client = ProtocolClient.createClient()
 
         val bytes: ByteArray = client.convert(message)
 
-        Assertions.assertThat(bytes[0]).isEqualTo(0x02)
+        Assertions.assertThat(bytes[0]).isEqualTo(0x01)
         Assertions.assertThat(bytes[1]).isEqualTo(1)
         Assertions.assertThat(bytes.decodeToString(2, bytes.size)).isEqualTo(message)
     }
