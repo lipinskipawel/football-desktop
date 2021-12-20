cb6ec6601f3cadc63efbc38dc36da45932ab0c97
Pawel Lipinski <lipinskijob@gmail.com>
2021-12-20 11:35:20 +0100

    Disable test connecting to WebServer

--- src/test/kotlin/com/github/lipinskipawel/network/ConnectionServerTest.kt
+++ src/test/kotlin/com/github/lipinskipawel/network/ConnectionServerTest.kt
@@ -1,14 +1,16 @@
 package com.github.lipinskipawel.network
 
 import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import java.net.URI
 
 class ConnectionServerTest {
 
     @Test
+    @Disabled
     fun `should connect to WebServer`() {
-        val connection = ConnectionManager.connectTo(URI.create("ws://localhost:8080/chat/one"))
+        val connection = ConnectionManager.connectTo(URI.create("ws://localhost:8080/lobby"))
 
         val isOpen = connection.isOpen()
 
