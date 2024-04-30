2f7ba7ae1a3cb9a6fe3490632576f12d3cd13eed
Pawel Lipinski <lipinskijob@gmail.com>
2024-04-30 14:51:40 +0200

    Clean up app:build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,17 @@
 plugins {
     application
     java
-    kotlin("jvm") version "1.6.21"
-    `maven-publish`
+    kotlin("jvm") version "1.9.23"
     id("com.github.johnrengelman.shadow") version "7.0.0"
     id("com.github.lipinskipawel.football-desktop.app-builder") version ("0.1.0")
 }
 
 repositories {
     mavenLocal()
-    maven {
-        url = uri("https://repo.maven.apache.org/maven2/")
-    }
+    mavenCentral()
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("com.github.lipinskipawel:game-engine:5.0.0")
     implementation("org.slf4j:slf4j-api:2.0.0-alpha1")
@@ -32,25 +28,8 @@ group = "com.github.lipinskipawel"
 version = "1.0.0"
 description = "football-desktop"
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions {
-        kotlinOptions.jvmTarget = "17"
-    }
-}
-
-publishing {
-    publications.create<MavenPublication>("maven") {
-        from(components["java"])
-    }
-}
-
-tasks.withType<JavaCompile>() {
-    options.encoding = "UTF-8"
+kotlin {
+    jvmToolchain(17)
 }
 
 application {
